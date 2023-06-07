438446928802f658c5f6bffc8b94fd2bd54aba14
Admin <Admin@DESKTOP-2NSIB7R>
2023-06-07 14:45:31 +0530

    hey guys please check

---
+++ src/test/java/Demo/Tc1.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Tc1 {
+
+	public static void main(String[] args) {
+	
+		
+		
+		System.out.println("helo bhai");
+		System.out.println("helo selenium");
+		System.out.println("helo pyhton");
+	}
+
+}
